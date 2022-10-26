9751f49c6974c22369dad0585934509738e7858e
박은빈 <joypeb@naver.com>
2022-10-26 15:47:39 +0900

    RequestMapping, GetMapping

--- src/main/java/com/example/hello/controller/HelloController.java
+++ src/main/java/com/example/hello/controller/HelloController.java
@@ -1,13 +1,29 @@
 package com.example.hello.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/vi/get-api")
 public class HelloController {
 
     @RequestMapping("/hello")
+    //그냥 RequestMapping은 모든 메소드(GET, POST...)가 다 가능하다
     public String hello() {
         return "Hello";
+    }
+
+    @RequestMapping(value = "/hello2", method = RequestMethod.GET)
+    public String hello2() {
+        return "hello GET";
+    }
+
+    @GetMapping(value = "/hello3")
+    public String hello3() {
+        return "hello GetMapping";
+    }
+
+    @GetMapping(value = "/variable1/{variable}")
+    public String getVariable1(@PathVariable String variable) {
+        return "hello " + variable;
     }
 }
