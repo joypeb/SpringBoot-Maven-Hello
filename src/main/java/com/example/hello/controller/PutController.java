68a2cd7c8491649b1efe66dc312ccee0344c9874
박은빈 <joypeb@naver.com>
2022-10-27 21:00:57 +0900

    ResponsEntity

--- src/main/java/com/example/hello/controller/PutController.java
+++ src/main/java/com/example/hello/controller/PutController.java
@@ -1,9 +1,12 @@
 package com.example.hello.controller;
 
 import com.example.hello.domain.dto.MemberDto;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/api/vi/put-api")
@@ -11,6 +14,16 @@ public class PutController {
 
     @PostMapping("member3")
     public ResponseEntity<MemberDto> putMember3(@RequestBody MemberDto memberDto) {
-        return ResponseEntity.status(HttpStatus.ACCEPTED).body(memberDto);
+        return ResponseEntity.status(HttpStatus.OK).body(memberDto);
+    }
+
+    @PostMapping("member4")
+    public ResponseEntity<String> putMember4(@RequestBody MemberDto memberDto) {
+        URI location = URI.create("locationTest");
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.setLocation(location);
+        responseHeaders.set("MyResponseHeader", "MyValue");
+        responseHeaders.set("test1","test2");
+        return new ResponseEntity<String>("bodybody", responseHeaders, HttpStatus.CREATED);
     }
 }
