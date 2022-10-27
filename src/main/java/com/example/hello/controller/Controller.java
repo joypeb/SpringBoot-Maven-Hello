d57223a5566a3dfeb1bea2a782b26263db9871b5
박은빈 <joypeb@naver.com>
2022-10-27 14:37:07 +0900

    ResponseEntity

--- src/main/java/com/example/hello/controller/Controller.java
+++ src/main/java/com/example/hello/controller/Controller.java
@@ -1,6 +1,8 @@
 package com.example.hello.controller;
 
 import com.example.hello.domain.dto.MemberDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -68,5 +70,8 @@ public class Controller {
         return memberDto.toString();
     }
 
-
+    @PostMapping("member3")
+    public ResponseEntity<MemberDto> putMember3(@RequestBody MemberDto memberDto) {
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(memberDto);
+    }
 }
